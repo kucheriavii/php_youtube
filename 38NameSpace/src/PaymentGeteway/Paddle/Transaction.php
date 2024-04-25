b2eb1b4fb745d3bf4976f48b9838f1d55c87dea3
r.ostapchuk <kucheriavii@gmail.com>
2024-04-25 22:13:21 +0300

    PHP Namespace Tutorial - Full PHP 8 Tutorial

--- 38NameSpace/src/PaymentGeteway/Paddle/Transaction.php
+++ 38NameSpace/src/PaymentGeteway/Paddle/Transaction.php
@@ -2,8 +2,17 @@
 
 declare(strict_types = 1);
 
-namespace PaymentGateWay\Paddle; 
+namespace PaymentGateWay\Paddle;
 
 class Transaction{
-
+    public function __construct(){
+        var_dump(new DateTime()); //DateTime custom from folder
+        echo "<br>";
+        var_dump(new \DateTime()); //global DateTime
+        echo "<br>";
+        var_dump(new \Notification\Email());
+        echo "<br>";
+        var_dump(explode(',',"Hello,world")); //global DateTime
+        echo "<br>";
+    }
 };
--- 38NameSpace/src/public/index.php
+++ 38NameSpace/src/public/index.php
@@ -1,6 +1,16 @@
 <?php
 
 require_once '../PaymentGeteway/Stripe/Transaction.php';
+require_once '../PaymentGeteway/Paddle/CustomerProfile.php';
+require_once '../PaymentGeteway/Paddle/DateTime.php';
+require_once '../PaymentGeteway/Paddle/Notification/Email.php';
 require_once '../PaymentGeteway/Paddle/Transaction.php';
 
-var_dump(new \PaymentGateWay\Stripe\Transaction());
+//first method using full path
+// var_dump(new \PaymentGateWay\Stripe\Transaction());
+
+
+//second method. Using "use" keyword
+use \PaymentGateWay\Paddle\Transaction;
+
+var_dump(new Transaction());

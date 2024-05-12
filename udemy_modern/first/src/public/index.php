9284ec3b80ae26f78dadad4b588e9f2c8908eb6d
r.ostapchuk <kucheriavii@gmail.com>
2024-05-12 21:33:02 +0300

    easy tasks

--- udemy_modern/first/src/public/index.php
+++ udemy_modern/first/src/public/index.php
@@ -10,4 +10,44 @@
 <body>
 <h1>Start project</h1>
 </body>
+<?php
+
+//color resistors task
+function getColor(string $color): int|string {
+    $colorMarkers = ["Green"=>1, "Red"=>2, "Blue"=>3, "Yellow"=>4];
+    if (array_key_exists($color, $colorMarkers)){
+        return $colorMarkers[$color];
+    }  else {
+        return "no such color";
+    }
+}
+
+//one for you task
+
+function oneFor(string|bool $name=False):string{
+    if($name){
+        return "one for $name one for me";
+    } else {
+        return "one for you one for me";
+    }
+}
+
+function leapYear(int $year):bool{
+    if (($year%4==0 and $year%100!=0) or $year%400==0) {
+        return True;
+    } else {
+        return False;
+    }
+}
+
+echo(getColor("Red"));
+echo("<br>");
+echo(oneFor("Tommy"));
+echo("<br>");
+echo(oneFor());
+echo("<br>");
+echo leapYear(1904);
+
+
+?>
 </html>

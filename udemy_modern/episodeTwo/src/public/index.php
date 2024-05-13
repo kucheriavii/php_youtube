8de4da7dcdfcfe66fb32375546dfaf540ae41965
r.ostapchuk <kucheriavii@gmail.com>
2024-05-13 21:22:23 +0300

    2. Alternative Syntax for Constants.mp4

--- udemy_modern/episodeTwo/src/public/index.php
+++ udemy_modern/episodeTwo/src/public/index.php
@@ -11,10 +11,16 @@
 <h1>Start project</h1>
 </body>
 <?php
-    echo PHP_VERSION . "<br>";
-    echo PHP_INT_MAX . "<br>";
-    echo PHP_INT_MIN . "<br>";
-    echo __FILE__ . "<br>";
-    echo __LINE__ . "<br>";
+//    echo PHP_VERSION . "<br>";
+//    echo PHP_INT_MAX . "<br>";
+//    echo PHP_INT_MIN . "<br>";
+//    echo __FILE__ . "<br>";
+//    echo __LINE__ . "<br>";
+
+    if(!defined("FOO")){
+        define('FOO', 'Hello world');
+    }
+    echo  FOO;
+
 ?>
 </html>
